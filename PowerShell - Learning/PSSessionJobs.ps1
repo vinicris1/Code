eee593bcbee1366f7aaf60e0aaa4fbdcf86bffb8
vinicris1 <viniciuscris277@gmail.com>
2023-09-27 21:38:53 -0300

    PowerShell - Learning

---
+++ PowerShell - Learning/PSSessionJobs.ps1
@@ -0,0 +1,32 @@
+﻿Clear-Host
+
+$s = New-PSSession -VMName Server-Learn
+
+Invoke-Command -Session $s -ScriptBlock {
+$i = 0
+while($true){
+$i++
+Write-Host "Count $i"
+Sleep 1
+if($i -ge 1000){
+break
+}
+}
+} -AsJob -JobName LongWork1
+
+
+Get-Command *PSSession
+
+Get-PSSession
+
+Remove-PSSession $s
+
+Disconnect-PSSession $s
+Receive-PSSession $s
+
+
+Get-Job LongWork1
+
+Get-Job LongWork1 | Receive-Job
+
+Get-Job Longwork1 | Stop-Job
