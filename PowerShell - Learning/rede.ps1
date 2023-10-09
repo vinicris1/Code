4743b33eaf7cd202a7d79b37a89a5fadb3f537b1
vinicris1 <viniciuscris277@gmail.com>
2023-10-09 20:24:41 -0300

    PowerShell - Learning

---
+++ PowerShell - Learning/rede.ps1
@@ -0,0 +1,13 @@
+﻿Clear-Host
+
+#também pode ser utilizado PSWA para acessar via internet
+
+$cred = Get-Credential users\administrator
+$rede = New-PSSession -ComputerName (Get-Content 'E:\Desktop\Code\PowerShell - Learning\servers.txt' ) -Credential $cred
+
+#Utilizando get-service em todos os computadores da rede, informados no servers.txt
+
+Invoke-Command $rede {Get-Service}
+
+$info = Get-WmiObject -Class Win32_ComputerSystem
+$info.Name
---
+++ PowerShell - Learning/remoteConnect.ps1
@@ -0,0 +1,11 @@
+﻿Set-Item WSMan:\localhost\Client\TrustedHosts 'WIN-AUP71KNM6F7'
+
+Get-Item WSMan:\localhost\Client\TrustedHosts
+
+Test-Connection WIN-AUP71KNM6F7
+
+$cred = Get-Credential users\administrator
+
+New-PSSession -ComputerName WIN-AUP71KNM6F7 -Credential $cred
+
+Enter-PSSession -Id 2
